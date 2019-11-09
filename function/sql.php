60bf87f05fb59355ae915a113b6315d71e08abd3
NguyenHuyenTrang070899 <37436902+NguyenHuyenTrang070899@users.noreply.github.com>
2019-11-09 22:49:58 +0700

    Update sql.php

--- function/sql.php
+++ function/sql.php
@@ -2,7 +2,7 @@
 //$con=mysql_connect('localhost','root','');
 	$domain = "/ShopWeb";
 	$con=mysqli_connect('localhost','root','') or die('canot connect to server');
-	mysqli_select_db($con,'demo') or die('canot select database');
+	mysqli_select_db($con,'shopweb') or die('canot select database');
 
 	$items = 0;
 	$pice = 0;
