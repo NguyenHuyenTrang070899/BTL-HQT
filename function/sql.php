c38343f12169cb83f51aadb5fe3f5529d7731847
NguyenHuyenTrang070899 <trang12345.vn@gmail.com>
2019-11-09 22:27:22 +0700

    Version 1.1

---
+++ function/sql.php
@@ -0,0 +1,9 @@
+<?php
+//$con=mysql_connect('localhost','root','');
+	$domain = "/ShopWeb";
+	$con=mysqli_connect('localhost','root','') or die('canot connect to server');
+	mysqli_select_db($con,'demo') or die('canot select database');
+
+	$items = 0;
+	$pice = 0;
+?>
